dd4d480ff1d31a7c6e62e81fa3780f3088fc1a43
Mshary <themshary@Msharys-MacBook-Pro.local>
2020-01-19 21:07:27 +0300

    Plan create feature works (tested w/ Postman) - Plan model (w/ __str__() method) - Plan view (only POST) - Plan model serializer - Plan URL - Plan model registered in admin site - Plan model migrations file
    + installed DRF
    + added requirements file

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Plan(models.Model):
+    creator = models.CharField(max_length=105)
+    location = models.CharField(max_length=105)
+    datetime = models.DateTimeField()
+    activity = models.TextField()
+
+    def __str__(self):
+        return f"{self.datetime} with {self.creator}"
--- core/views.py
+++ core/views.py
@@ -1,3 +1,18 @@
+# Django imports
 from django.shortcuts import render
 
-# Create your views here.
+# Django REST framework imports
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+# App imports
+from .serializers import PlanSerializer
+
+class PlanView(APIView):
+    def post(self, request):
+        serializer = PlanSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
