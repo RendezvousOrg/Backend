51b887c546aaf40555a49509b15b5c4f50640a3d
GhalyahF <gaa291@nyu.edu>
2020-02-08 19:38:37 +0300

    added attendees m2m

--- core/models.py
+++ core/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Plan(models.Model):
     creator = models.CharField(max_length=105)
     location = models.CharField(max_length=105)
     datetime = models.CharField(max_length=105)
     activity = models.TextField()
+    attendees = models.ManyToManyField(User)
 
     def __str__(self):
         return f"{self.datetime} with {self.creator}"
